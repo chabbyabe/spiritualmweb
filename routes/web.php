eceec34ed5189e26ecfc13e3a8794bd33352faba
Abegail Isidro <chabbyabe@gmail.com>
2026-04-27 19:13:28 +0200

    Add redirect to spiritual m

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,17 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::middleware(['web'])->group(function () {
+    // Route::get('/', function () {
+    //     return Inertia::render('Home/Home', [
+    //         'canLogin' => Route::has('login'),
+    //         'canRegister' => Route::has('register'),
+    //         'laravelVersion' => Application::VERSION,
+    //         'phpVersion' => PHP_VERSION,
+    //     ]);
+    // })->name('home');
     Route::get('/', function () {
-        return Inertia::render('Home/Home', [
-            'canLogin' => Route::has('login'),
-            'canRegister' => Route::has('register'),
-            'laravelVersion' => Application::VERSION,
-            'phpVersion' => PHP_VERSION,
-        ]);
+        return redirect()->away('https://spiritualmaturity.nl/', 301);
     })->name('home');
-
     Route::get('/about', function () {
         return Inertia::render('Home/About');
     })->name('home.about');
@@ -29,6 +31,11 @@ Route::middleware(['web'])->group(function () {
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/devotions', function () {
+    return Inertia::render('Devotions');
+})->middleware(['auth', 'verified'])->name('devotions');
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -36,4 +43,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+ Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
+     \UniSharp\LaravelFilemanager\Lfm::routes();
+ });
+ 
 require __DIR__.'/auth.php';
